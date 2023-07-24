9d36b098b46bf68f1b515b5f3a10224294f7e223
inthree3 <inseon_ug@gm.gist.ac.kr>
2023-07-24 12:42:01 +0900

    0721_tracking_the_ball

--- select_color.py
+++ select_color.py
@@ -1,8 +1,41 @@
 import cv2
 import numpy as np
+import matplotlib as plt
 cap=cv2.VideoCapture(1)
 
 i=0
+
+# range of orange color
+lower_color = np.array([18, 71, 100])
+upper_color = np.array([25, 88, 100])
+
+#define initial cX, cY
+cX=-100
+cY=-100
+
+def plot_cX_cY(cX, cY):
+    Xs=[]
+    Ys=[]
+
+    Xs.append(cX)
+    Ys.append(cY)
+
+    plt.plot(Xs, Ys)
+
+
+#select the color through clicking
+def click_color(event, x, y, flags, params):
+    global lower_color, upper_color
+    if event == cv2.EVENT_LBUTTONDOWN:
+        selected_color=frame[y, x]
+        hsv_color=cv2.cvtColor(np.uint8([[selected_color]]), cv2.COLOR_BGR2HSV)
+        hue=hsv_color[0][0][0]
+        shade=hsv_color[0][0][1]
+        value=hsv_color[0][0][2]
+        lower_color=np.array([hue-10, shade-10, value-10])
+        upper_color=np.array([hue+10, shade+10, value+10])
+        print("Selected HSV Range is ",lower_color, upper_color)
+
 while True:
     ret, frame=cap.read()
     if not ret:
@@ -10,10 +43,6 @@ while True:
 
     #convert the color from bgr to hsv
     hsv=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    #range of orange color
-    lower_color = np.array([10, 100, 100])
-    upper_color = np.array([25, 255, 255])
     
     #generate the mask
     mask = cv2.inRange(hsv, lower_color, upper_color)
@@ -35,22 +64,25 @@ while True:
             cX = int(moments["m10"] / moments["m00"])
             cY = int(moments["m01"] / moments["m00"])
 
-        circle=cv2.circle(hsv, (cX, cY), 5, (0, 255, 0), -1)
-    
+            circle=cv2.circle(hsv, (cX, cY), 5, (0, 255, 0), -1)
+            
+
+        
+        
     if i%100==0:
         print(f"cX: {cX}, cY: {cY}")
-        i=0
+        i+=1
 
     cv2.imshow("frame", hsv)
     cv2.imshow("mask", mask)
-    cv2.imshow("circle", circle)
+    
+    cv2.setMouseCallback("frame", click_color)
 
+    i+=1
 
     #'p' for existing the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-    i+=1
 
 cap.release()
 cv2.destroyAllWindows()
