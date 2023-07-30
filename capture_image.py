810a2cc942711ce2314a52a97315709c620de49a
inthree3 <inseon_ug@gm.gist.ac.kr>
2023-07-30 17:21:00 +0900

    chore: change image frame setting

--- capture_image.py
+++ capture_image.py
@@ -4,6 +4,9 @@ import cv2
 num=0
 cap=cv2.VideoCapture(num)
 
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
 ret,frame=cap.read()
 
 cv2.imwrite(f'cam{num}_ex.jpg', frame)
